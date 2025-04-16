dfca876416cc988b92a92b3905c9c4f18e5e63fb
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-16 14:44:26 -0300

    refactor: alteração nos parametros de retorno do mapa

--- frontend/src/components/mapa/MapaBrasil.jsx
+++ frontend/src/components/mapa/MapaBrasil.jsx
@@ -1,9 +1,17 @@
 import Norte from './regiao/Norte';
+import Nordeste from './regiao/Nordeste';
+import CentroOeste from './regiao/CentroOeste';
+import Sudeste from './regiao/Sudeste';
+import Sul from './regiao/Sul';
 
 const MapaBrasil = () => {
     return(
-        <ul id='map'>
+        <ul id="map">
             <Norte/>
+            <Nordeste/>
+            <CentroOeste/>
+            <Sudeste/>
+            <Sul/>
         </ul>
     );
 };
