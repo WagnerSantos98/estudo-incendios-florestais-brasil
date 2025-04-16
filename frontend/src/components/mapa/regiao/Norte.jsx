8c7a6f0da0448e1ad809eea349b208e92c8dcc9b
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-16 14:38:56 -0300

    refactor: alteração na chamada da região

--- frontend/src/components/mapa/regiao/Norte.jsx
+++ frontend/src/components/mapa/regiao/Norte.jsx
@@ -1,12 +1,14 @@
+import { estadosPorRegiao } from "../../../utils/estadosPorRegiao";
+
 const Norte = () => (
   <>
-    <li id="cac"><a href="#" id="ac" title="Acre">AC</a></li>
-    <li id="cap"><a href="#" id="ap" title="Amapá">AP</a></li>
-    <li id="cam"><a href="#" id="am" title="Amazonas">AM</a></li>
-    <li id="cpa"><a href="#" id="pa" title="Pará">PA</a></li>
-    <li id="cro"><a href="#" id="ro" title="Rondônia">RO</a></li>
-    <li id="crr"><a href="#" id="rr" title="Roraima">RR</a></li>
-    <li id="cto"><a href="#" id="to" title="Tocantins">TO</a></li>
+    {estadosPorRegiao.norte.map((estado) => {
+      <li key={estado.idLi} id={estado.idLi}>
+        <a href="#" id={estado.idA} title={estado.nome}>
+          {estado.idA.toUpperCase}
+        </a>
+      </li>
+    })}
   </>  
 );
 
