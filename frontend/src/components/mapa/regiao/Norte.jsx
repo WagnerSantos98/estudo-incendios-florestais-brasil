a85f1670a754d57fb3ccd6ba005cd84ca48765b3
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-16 14:28:52 -0300

    feat: inserção do componente Norte

---
+++ frontend/src/components/mapa/regiao/Norte.jsx
@@ -0,0 +1,13 @@
+const Norte = () => (
+  <>
+    <li id="cac"><a href="#" id="ac" title="Acre">AC</a></li>
+    <li id="cap"><a href="#" id="ap" title="Amapá">AP</a></li>
+    <li id="cam"><a href="#" id="am" title="Amazonas">AM</a></li>
+    <li id="cpa"><a href="#" id="pa" title="Pará">PA</a></li>
+    <li id="cro"><a href="#" id="ro" title="Rondônia">RO</a></li>
+    <li id="crr"><a href="#" id="rr" title="Roraima">RR</a></li>
+    <li id="cto"><a href="#" id="to" title="Tocantins">TO</a></li>
+  </>  
+);
+
+export default Norte
