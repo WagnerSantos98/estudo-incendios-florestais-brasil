58d91450819b06f46da24119f4f849b431bbd556
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-21 14:11:32 -0300

    refactor: alteração no componente mapa das regiões

--- frontend/src/components/mapa/regiao/Sudeste.jsx
+++ frontend/src/components/mapa/regiao/Sudeste.jsx
@@ -1,14 +1,21 @@
 import { estadosPorRegiao } from "../../../utils/estadosPorRegiao";
+import '../styles/sudeste.css';
 
-const Sudeste = () => (
+const Sudeste = ({ onMouseEnter, onMouseLeave }) => (
   <>
-    {estadosPorRegiao.sudeste.map((estado) => {
+    {estadosPorRegiao.sudeste.map((estado) => (
       <li key={estado.idLi} id={estado.idLi}>
-        <a href="#" id={estado.idA} title={estado.nome}>
-          {estado.idA.toUpperCase}
+        <a 
+          href="#" 
+          id={estado.idA} 
+          title={estado.nome}
+          onMouseEnter={(e) => onMouseEnter(estado, e)}
+          onMouseLeave={onMouseLeave}
+        >
+          <img src="../../assets/img/null.gif" alt={estado.idA.toUpperCase()} />
         </a>
       </li>
-    })}
+    ))}
   </>  
 );
 
--- frontend/src/components/mapa/regiao/Sul.jsx
+++ frontend/src/components/mapa/regiao/Sul.jsx
@@ -1,14 +1,21 @@
 import { estadosPorRegiao } from "../../../utils/estadosPorRegiao";
+import '../styles/sul.css';
 
-const Sul = () => (
+const Sul = ({ onMouseEnter, onMouseLeave }) => (
   <>
-    {estadosPorRegiao.sul.map((estado) => {
+    {estadosPorRegiao.sul.map((estado) => (
       <li key={estado.idLi} id={estado.idLi}>
-        <a href="#" id={estado.idA} title={estado.nome}>
-          {estado.idA.toUpperCase}
+        <a 
+          href="#" 
+          id={estado.idA} 
+          title={estado.nome}
+          onMouseEnter={(e) => onMouseEnter(estado, e)}
+          onMouseLeave={onMouseLeave}
+        >
+          <img src="../../assets/img/null.gif" alt={estado.idA.toUpperCase()} />
         </a>
       </li>
-    })}
+    ))}
   </>  
 );
 
