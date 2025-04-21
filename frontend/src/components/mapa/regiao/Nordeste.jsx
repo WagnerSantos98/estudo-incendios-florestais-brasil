6625c28e430444715903cc944320e2e6482edcac
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-21 14:08:08 -0300

    refactor: alteração no componente mapa das regiões

--- frontend/src/components/mapa/regiao/Nordeste.jsx
+++ frontend/src/components/mapa/regiao/Nordeste.jsx
@@ -1,14 +1,21 @@
 import { estadosPorRegiao } from "../../../utils/estadosPorRegiao";
+import '../styles/nordeste.css';
 
-const Nordeste = () => (
+const Nordeste = ({ onMouseEnter, onMouseLeave }) => (
   <>
-    {estadosPorRegiao.nordeste.map((estado) => {
+    {estadosPorRegiao.nordeste.map((estado) => (
       <li key={estado.idLi} id={estado.idLi}>
-        <a href="#" id={estado.idA} title={estado.nome}>
-          {estado.idA.toUpperCase}
+        <a 
+          href="#" 
+          id={estado.idA} 
+          title={estado.nome}
+          onMouseEnter={(e) => onMouseEnter(estado, e)}
+          onMouseLeave={onMouseLeave}
+        >
+          <img src="../../assets/img/null.gif" alt={estado.idA.toUpperCase()} />
         </a>
       </li>
-    })}
+    ))}
   </>  
 );
 
