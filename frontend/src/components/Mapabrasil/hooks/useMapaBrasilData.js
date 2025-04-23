ca554cb659065ac3a55a87ce3b58b5a96c3d7507
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-23 18:42:41 -0300

    fea: implementação de hooks para comunicação com backend

--- frontend/src/components/Mapabrasil/hooks/useMapaBrasilData.js
+++ frontend/src/components/Mapabrasil/hooks/useMapaBrasilData.js
@@ -1,2 +1,43 @@
 import { useState, useEffect } from 'react';
-import ApiService from '../../../services/'
+import ApiService from '../../../services/ApiService';
+
+export const useMapaBrasilData = () => {
+    const [currentData, setCurrentData] = useState(null);
+    const [chartData, setChartData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [analiseDados, setAnaliseDados] = useState(null);
+    const [estatisticasGeral, setEstatisticasGeral] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const [geralRes, graficoRes, analiseRes] = await Promise.all([
+                    ApiService.getEstatisticasGeral(),
+                    ApiService.getEstatisticasGrafico(),
+                    ApiService.getAnaliseDescricao()
+                ]);
+
+                setEstatisticasGeral(geralRes);
+                setCurrentData(geralRes);
+                setChartData(graficoRes);
+                setAnaliseDados(analiseRes);
+
+                setLoading(false);
+            }catch(error){
+                console.error("Erro ao carregar dados:", error);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return{
+        currentData,
+        chartData,
+        loading,
+        analiseDados,
+        estatisticasGeral,
+        setCurrentData
+    };
+};
