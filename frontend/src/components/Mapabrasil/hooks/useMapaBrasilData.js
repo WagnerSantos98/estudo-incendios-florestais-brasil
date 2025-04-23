7f81e90bc97737332c40121c56a7e27c448c8861
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-04-23 20:38:45 -0300

    refactor: alteração do hooks mapaBrasilData

--- frontend/src/components/Mapabrasil/hooks/useMapaBrasilData.js
+++ frontend/src/components/Mapabrasil/hooks/useMapaBrasilData.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import ApiService from '../../../services/ApiService';
+import { estadosPorRegiao } from '../../../utils/estadosPorRegiao';
 
 export const useMapaBrasilData = () => {
     const [currentData, setCurrentData] = useState(null);
@@ -7,6 +8,9 @@ export const useMapaBrasilData = () => {
     const [loading, setLoading] = useState(true);
     const [analiseDados, setAnaliseDados] = useState(null);
     const [estatisticasGeral, setEstatisticasGeral] = useState(null);
+    const [estadoSelecionado, setEstadoSelecionado] = useState(null);
+    const [regiaoSelecionada, setRegiaoSelecionada] = useState('');
+    const [estadosFiltrados, setEstadosFiltrados] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -32,12 +36,44 @@ export const useMapaBrasilData = () => {
         fetchData();
     }, []);
 
+    const handleRegiaoChange = (regiao) => {
+        setRegiaoSelecionada(regiao);
+        if(regiao){
+            setEstadosFiltrados(estadosPorRegiao[regiao]);
+        }else{
+            setEstadosFiltrados([]);
+        }
+        setEstadoSelecionado(null);
+        setCurrentData(estatisticasGeral);
+    }
+
+    const handleEstadoChange = async (estadoId) => {
+        if(estadoId){
+            try{
+                const response = ApiService.getEstatisticasPorEstado();
+                setCurrentData(response.data);
+                setEstadoSelecionado(estadoId);
+            }catch(error){
+                console.error("Erro ao carregar dados do estado:", error);
+            }
+        }else{
+            setCurrentData(estatisticasGeral);
+            setEstadoSelecionado(null);
+        }
+    }
+
     return{
         currentData,
         chartData,
         loading,
         analiseDados,
         estatisticasGeral,
-        setCurrentData
+        estadoSelecionado,
+        estadosFiltrados,
+        regiaoSelecionada,
+        setCurrentData,
+        setEstadoSelecionado,
+        handleRegiaoChange,
+        handleEstadoChange
     };
 };
